56f88b644c02d240bab60118378cb11c29abef4c
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2025-04-14 05:52:51 +0900

    enable to map json to Repo

--- app/src/main/java/com/example/beginnertrainingandroid2025/data/Repo.kt
+++ app/src/main/java/com/example/beginnertrainingandroid2025/data/Repo.kt
@@ -1,8 +1,12 @@
 package com.example.beginnertrainingandroid2025.data
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Repo(
     val id: Int,
     val name: String,
     val description: String? = null,
-    val stars: Int,
+    @SerialName("stargazers_count") val stars: Int,
 )
