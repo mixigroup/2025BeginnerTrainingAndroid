46ef4045f64fa24ddc3007350d03f1e8571a9dfd
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2025-04-14 02:55:15 +0900

    impl RepoListItem

--- app/src/main/java/com/example/beginnertrainingandroid2025/ui/theme/RepoListItem.kt
+++ app/src/main/java/com/example/beginnertrainingandroid2025/ui/theme/RepoListItem.kt
@@ -1,8 +1,18 @@
 package com.example.beginnertrainingandroid2025.ui.theme
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.beginnertrainingandroid2025.data.Repo
 
 @Composable
@@ -10,7 +20,24 @@ fun RepoListItem(
     repo: Repo,
     modifier: Modifier = Modifier,
 ) {
-
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+    ) {
+        Text(
+            text = repo.name,
+            fontWeight = FontWeight.Bold,
+        )
+        repo.description?.let { Text(text = it) }
+        Row {
+            Icon(
+                imageVector = Icons.Outlined.Star,
+                tint = Color.LightGray,
+                contentDescription = null,
+            )
+            Text(text = "${repo.stars}")
+        }
+    }
 }
 
 @Preview(showBackground = true)
