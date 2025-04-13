6e8f24c70ab78d47c54149d2fbc100920a9c606f
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2025-04-14 02:46:33 +0900

    add I/F

---
+++ app/src/main/java/com/example/beginnertrainingandroid2025/ui/theme/RepoListItem.kt
@@ -0,0 +1,14 @@
+package com.example.beginnertrainingandroid2025.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.beginnertrainingandroid2025.data.Repo
+
+@Composable
+fun RepoListItem(
+    repo: Repo,
+    modifier: Modifier = Modifier,
+) {
+
+}
