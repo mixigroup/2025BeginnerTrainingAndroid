7afd4bcef49bf941aba99deb33ccd40784d3fc87
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2025-04-14 04:57:29 +0900

    relocate

--- app/src/main/java/com/example/beginnertrainingandroid2025/ui/RepoListItem.kt
+++ app/src/main/java/com/example/beginnertrainingandroid2025/ui/RepoListItem.kt
@@ -1,4 +1,4 @@
-package com.example.beginnertrainingandroid2025.ui.theme
+package com.example.beginnertrainingandroid2025.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,6 +14,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.beginnertrainingandroid2025.data.Repo
+import com.example.beginnertrainingandroid2025.ui.theme.BeginnerTrainingAndroid2025Theme
 
 @Composable
 fun RepoListItem(
