74835e1e47c4e2e96a371898d49472154ce78e78
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2025-04-14 05:05:50 +0900

    show list of repos

---
+++ app/src/main/java/com/example/beginnertrainingandroid2025/ui/HomeScreen.kt
@@ -0,0 +1,44 @@
+package com.example.beginnertrainingandroid2025.ui
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.beginnertrainingandroid2025.data.Repo
+import com.example.beginnertrainingandroid2025.ui.theme.BeginnerTrainingAndroid2025Theme
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+@Composable
+fun HomeScreen(
+    repos: List<Repo>,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(modifier = modifier) {
+        items(
+            items = repos,
+            key = { it.id },
+        ) {
+            RepoListItem(repo = it)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeScreenPreview() {
+    BeginnerTrainingAndroid2025Theme {
+        val repos = List(1000) {
+            Repo(
+                id = it,
+                name = "repo$it",
+                description = if (it.mod(2) == 0) "This is awesome repository" else null,
+                stars = Random.nextInt(IntRange(0, 1000)),
+            )
+        }
+        HomeScreen(
+            repos = repos,
+        )
+    }
+}
