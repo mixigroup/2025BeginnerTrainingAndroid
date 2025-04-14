7f1f05f744801358bb9337cb80b414ccffaab036
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2025-04-15 03:49:59 +0900

    impl bookmark screen

--- composeApp/src/commonMain/kotlin/com/example/beginnertrainingandroid2025/ui/BookmarkScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/beginnertrainingandroid2025/ui/BookmarkScreen.kt
@@ -1,7 +1,93 @@
 package com.example.beginnertrainingandroid2025.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.beginnertrainingandroid2025.data.Repo
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+data class BookmarkScreenUiState(
+    val items: List<Repo>
+)
 
 @Composable
-fun BookmarkScreen(modifier: Modifier = Modifier) {}
+fun BookmarkScreen(
+    viewModel: BookmarkViewModel = viewModel(factory = BookmarkViewModel.Factory),
+    modifier: Modifier = Modifier,
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    LaunchedEffect(Unit) { viewModel.onLaunched() }
+
+    BookmarkScreen(
+        modifier = modifier,
+        onBookmarkIconClick = viewModel::onBookmarkIconClick,
+        uiState = uiState,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun BookmarkScreen(
+    uiState: BookmarkScreenUiState,
+    onBookmarkIconClick: (Repo) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "ブックマーク")
+                },
+            )
+        },
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(8.dp),
+        ) {
+            items(uiState.items) { repo ->
+                RepoListItem(
+                    repo = repo,
+                    isBookmarked = true,
+                    onBookmarkIconClick = onBookmarkIconClick,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun BookmarkScreenPreview() {
+    MaterialTheme {
+        val repos = List(1000) {
+            Repo(
+                id = it,
+                name = "repo$it",
+                description = if (it.mod(2) == 0) "This is awesome repository" else null,
+                stars = Random.nextInt(IntRange(0, 1000)),
+            )
+        }
+        BookmarkScreen(
+            uiState = BookmarkScreenUiState(items = repos),
+            onBookmarkIconClick = {},
+        )
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/example/beginnertrainingandroid2025/ui/BookmarkViewModel.kt
@@ -0,0 +1,57 @@
+package com.example.beginnertrainingandroid2025.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.beginnertrainingandroid2025.data.DefaultRepoRepository
+import com.example.beginnertrainingandroid2025.data.Repo
+import com.example.beginnertrainingandroid2025.data.RepoLocalDataSource
+import com.example.beginnertrainingandroid2025.data.RepoRemoteDataSource
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlin.reflect.KClass
+
+class BookmarkViewModel(
+    private val repository: DefaultRepoRepository,
+): ViewModel() {
+    var uiState = MutableStateFlow(
+        BookmarkScreenUiState(
+            items = emptyList(),
+        )
+    )
+        private set
+
+    fun onLaunched() {
+        viewModelScope.launch {
+            uiState.value = BookmarkScreenUiState(
+                items = repository.getBookmarkedRepoList(),
+            )
+        }
+    }
+
+    fun onBookmarkIconClick(repo: Repo) {
+        viewModelScope.launch {
+            repository.saveAsUnBookmark(repo)
+
+            uiState.update {
+                it.copy(items = repository.getBookmarkedRepoList())
+            }
+        }
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(modelClass: KClass<T>, extras: CreationExtras): T {
+                return BookmarkViewModel(
+                    repository = DefaultRepoRepository(
+                        localDataSource = RepoLocalDataSource(),
+                        remoteDataSource = RepoRemoteDataSource(),
+                    ),
+                ) as T
+            }
+        }
+    }
+}
