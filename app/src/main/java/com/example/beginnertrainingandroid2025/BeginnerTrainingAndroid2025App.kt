24f66fdb5a96afadabef5e7fcbb08e5c8d08e1e4
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2025-04-14 11:13:44 +0900

    add app composable

---
+++ app/src/main/java/com/example/beginnertrainingandroid2025/BeginnerTrainingAndroid2025App.kt
@@ -0,0 +1,9 @@
+package com.example.beginnertrainingandroid2025
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BeginnerTrainingAndroid2025(modifier: Modifier = Modifier) {
+
+}
