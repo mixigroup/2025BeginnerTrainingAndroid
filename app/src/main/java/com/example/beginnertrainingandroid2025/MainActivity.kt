d4aa3aa6f0e701228c05785166bce834ffc20215
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2025-04-14 11:30:11 +0900

    show bottom navigation bar

--- app/src/main/java/com/example/beginnertrainingandroid2025/MainActivity.kt
+++ app/src/main/java/com/example/beginnertrainingandroid2025/MainActivity.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.beginnertrainingandroid2025.ui.HomeScreen
-import com.example.beginnertrainingandroid2025.ui.theme.BeginnerTrainingAndroid2025Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BeginnerTrainingAndroid2025Theme {
-                HomeScreen()
-            }
+            BeginnerTrainingAndroid2025()
         }
     }
 }
---
+++ app/src/main/java/com/example/beginnertrainingandroid2025/ui/BookmarkScreen.kt
@@ -0,0 +1,7 @@
+package com.example.beginnertrainingandroid2025.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BookmarkScreen(modifier: Modifier = Modifier) {}
