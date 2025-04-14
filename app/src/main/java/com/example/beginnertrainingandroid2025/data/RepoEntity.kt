62f38fb0070224766839bedd78d36dc5406d175a
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2025-04-14 10:24:33 +0900

    impl mapper

--- app/src/main/java/com/example/beginnertrainingandroid2025/data/RepoEntity.kt
+++ app/src/main/java/com/example/beginnertrainingandroid2025/data/RepoEntity.kt
@@ -10,5 +10,23 @@ data class RepoEntity(
     @PrimaryKey val id: Int,
     val name: String,
     val description: String? = null,
+    val stars: Int,
 )
 
+fun RepoEntity.toModel() = Repo(
+    id = id,
+    name = name,
+    description = description,
+    stars = stars,
+)
+
+fun Repo.toEntity() = RepoEntity(
+    id = id,
+    name = name,
+    description = description,
+    stars = stars,
+)
+
+fun Repo.toBookmarkEntity() = BookmarkRepoEntity(
+    repoId = id,
+)
