8642d8c3504322010712b9efff263408560354a1
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2025-04-14 10:29:08 +0900

    fix repository

--- app/src/main/java/com/example/beginnertrainingandroid2025/data/RepoRepository.kt
+++ app/src/main/java/com/example/beginnertrainingandroid2025/data/RepoRepository.kt
@@ -1,7 +1,25 @@
 package com.example.beginnertrainingandroid2025.data
 
 class RepoRepository(
+    private val localDataSource: RepoLocalDataSource,
     private val remoteDataSource: RepoRemoteDataSource,
 ) {
-    suspend fun getRepos(): List<Repo> = remoteDataSource.getRepos()
-}
+    suspend fun getRepoList(): List<Repo> {
+        return localDataSource.getRepoList().ifEmpty {
+            val repoList = remoteDataSource.getRepoList()
+            localDataSource.saveRepoList(repoList)
+            repoList
+        }
+    }
+
+    suspend fun saveAsBookmark(repo: Repo) {
+        localDataSource.saveAsBookmark(repo)
+    }
+
+    suspend fun saveAsUnBookmark(repo: Repo) {
+        localDataSource.saveAsUnBookmark(repo)
+    }
+
+    suspend fun getBookmarkedRepoList(): List<Repo> {
+        return localDataSource.getBookmarkRepoList()
+    }}
