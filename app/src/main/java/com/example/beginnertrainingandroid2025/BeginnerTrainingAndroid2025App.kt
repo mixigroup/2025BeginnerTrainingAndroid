944b2e15d0662e921f177eb8f96ca8e954820b82
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2025-04-14 11:14:09 +0900

    add route type

--- app/src/main/java/com/example/beginnertrainingandroid2025/BeginnerTrainingAndroid2025App.kt
+++ app/src/main/java/com/example/beginnertrainingandroid2025/BeginnerTrainingAndroid2025App.kt
@@ -2,6 +2,13 @@ package com.example.beginnertrainingandroid2025
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Home // ホーム画面
+
+@Serializable
+object Bookmark // ブックマーク画面
 
 @Composable
 fun BeginnerTrainingAndroid2025(modifier: Modifier = Modifier) {
