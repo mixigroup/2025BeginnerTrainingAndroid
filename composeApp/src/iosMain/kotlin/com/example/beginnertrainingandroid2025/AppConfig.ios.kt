bf9f6b8ff707311a3ec8793316c63dfff4a177f7
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2025-04-15 09:51:59 +0900

    set api token

--- composeApp/src/iosMain/kotlin/com/example/beginnertrainingandroid2025/AppConfig.ios.kt
+++ composeApp/src/iosMain/kotlin/com/example/beginnertrainingandroid2025/AppConfig.ios.kt
@@ -1,6 +1,26 @@
 package com.example.beginnertrainingandroid2025
 
+import platform.Foundation.NSBundle
+import platform.Foundation.NSDictionary
+import platform.Foundation.dictionaryWithContentsOfFile
+
 actual object AppConfig {
     actual val GITHUB_API_TOKEN: String
-        get() = TODO("Not yet implemented")
+        get() = getStringResource(
+            filename = "Secrets",
+            fileType = "plist",
+            valueKey = "GITHUB_API_TOKEN"
+        ) ?: ""
+}
+
+internal fun getStringResource(
+    filename: String,
+    fileType: String,
+    valueKey: String,
+): String? {
+    val result = NSBundle.mainBundle.pathForResource(filename, fileType)?.let {
+        val map = NSDictionary.dictionaryWithContentsOfFile(it)
+        map?.get(valueKey) as? String
+    }
+    return result
 }
