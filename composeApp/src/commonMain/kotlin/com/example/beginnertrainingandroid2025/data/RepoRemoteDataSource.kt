03012af2f81c6bb40d7e304d08af282d76e308d6
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2025-04-15 09:52:12 +0900

    use http client

--- composeApp/src/commonMain/kotlin/com/example/beginnertrainingandroid2025/data/RepoRemoteDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/example/beginnertrainingandroid2025/data/RepoRemoteDataSource.kt
@@ -1,18 +1,13 @@
 package com.example.beginnertrainingandroid2025.data
 
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.withContext
 
 class RepoRemoteDataSource {
     suspend fun getRepoList(): List<Repo> = withContext(Dispatchers.IO) {
-        List(100) {
-            Repo(
-                id = it,
-                name = "repo$it",
-                description = "this is awesome",
-                stars = it,
-            )
-        }
+        httpClient.get("https://api.github.com/orgs/mixigroup/repos").body()
     }
 }
