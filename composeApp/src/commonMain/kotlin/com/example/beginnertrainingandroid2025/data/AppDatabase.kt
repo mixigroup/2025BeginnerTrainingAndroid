36d40674d457c2f71b2ee2c3305fa6cc3e9904bf
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2025-04-15 11:06:18 +0900

    make it run on ios

--- composeApp/src/commonMain/kotlin/com/example/beginnertrainingandroid2025/data/AppDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/example/beginnertrainingandroid2025/data/AppDatabase.kt
@@ -1,7 +1,9 @@
 package com.example.beginnertrainingandroid2025.data
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
 
 @Database(
     entities = [
@@ -10,6 +12,13 @@ import androidx.room.RoomDatabase
     ],
     version = 1,
 )
+@ConstructedBy(AppDatabaseConstructor::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun repoDao(): RepoDao
+}
+
+// The Room compiler generates the `actual` implementations.
+@Suppress("NO_ACTUAL_FOR_EXPECT")
+expect object AppDatabaseConstructor : RoomDatabaseConstructor<AppDatabase> {
+    override fun initialize(): AppDatabase
 }
--- composeApp/src/iosMain/kotlin/com/example/beginnertrainingandroid2025/data/getAppDatabase.kt
+++ composeApp/src/iosMain/kotlin/com/example/beginnertrainingandroid2025/data/getAppDatabase.kt
@@ -4,13 +4,13 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import platform.Foundation.NSHomeDirectory
 
 fun getAppDatabase(): RoomDatabase.Builder<AppDatabase> {
     val dbFile = NSHomeDirectory() + "/user.db"
     return Room.databaseBuilder<AppDatabase>(
         name = dbFile,
-        factory = { AppDatabase::class.instantiateImpl() }
     )
         .fallbackToDestructiveMigrationOnDowngrade(true)
         .setDriver(BundledSQLiteDriver())
